298af4dc85279c926e5a957359773ec6a4af88f1
SirojiddinJurayev <sirojiddinjurayev537@gmail.com>
2022-01-15 09:05:42 +0300

    bottom inners

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
   slidesToScroll: 4,
     });
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+  // const burgerBtn = document.querySelector(".header__mob-btn");
+  // const mobileMenu = document.querySelector("#mob__menu");
+  // const closeBtn = document.querySelector(".header__mob-close-btn");
+  
+  // burgerBtn.addEventListener("click", () => {
+  //   mobileMenu.classList.toggle("active");
+  //   burgerBtn.classList.toggle("falsy");
+  //   closeBtn.classList.toggle("show__close-btn");
+  // });
+  function classToggle() {
+    const navs = document.querySelectorAll('.navbar-items')
+    
+    navs.forEach(nav => nav.classList.toggle('navbar-toggleshow'));
+  }
+  
+  document.querySelector('.navbar-link-toggle')
+    .addEventListener('click', classToggle);
